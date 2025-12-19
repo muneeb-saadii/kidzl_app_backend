6f20324e54db726d80e512c251c3df4fe99efe45
Muneeb Saadii <muhammadmuneeb8992@gmail.com>
2025-12-19 17:33:39 +0500

    updated db env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 const routes = require('./routes/main_routes');
 app.use('/api', routes);
 
-const PORT = process.env.LOCAL_PORT || 5000;
+const PORT = process.env.LOCAL_PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
